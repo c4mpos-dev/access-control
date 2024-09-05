c39ff59d4f6c12dfd9e78afba89e76d6099667f3
c4mpos-dev <campos.eet@gmail.com>
2024-09-05 15:30:48 -0300

    Using StyleSheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,34 @@
-import { Fragment } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 
 export default function App() {
   return (
-    <View style={{
-      flex: 1,
-      backgroundColor: '#131016',
-      padding: 24
-    }}>
-      <Text style={{
-        color: '#FDFCFE',
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginTop: 48
-      }}>
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
         Campos's party
       </Text>
 
-      <Text style={{
-        color: '#6B6B6B'
-      }}>
+      <Text style={styles.eventDate}>
         Sexta-feira, 6 de Setembro de 2024.
       </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+  },
+  eventName: {
+    color: '#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 14
+  }
+});
