6f53086faedb4cbe5d1f89e46a55cb7376c7a4fa
c4mpos-dev <campos.eet@gmail.com>
2024-09-05 14:22:07 -0300

    Direct styling in the component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,28 @@
-import { Image, StyleSheet, Platform, View, Text } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
+import { Fragment } from 'react';
+import { Text, View } from 'react-native';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Cauã Campos</Text>
-      <StatusBar style="auto"/>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    }}>
+      <Text style={{
+        color: '#FDFCFE',
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 48
+      }}>
+        Campos's party
+      </Text>
+
+      <Text style={{
+        color: '#6B6B6B'
+      }}>
+        Sexta-feira, 6 de Setembro de 2024.
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#252C35',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color: 'white',
-    fontSize: 50
-  }
-});
