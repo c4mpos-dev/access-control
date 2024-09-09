d6a0a43aa8663894cfc96a5686f70c82695bc911
c4mpos-dev <campos.eet@gmail.com>
2024-09-09 16:32:10 -0300

    Properties with functions

--- app/screens/Home/index.tsx
+++ app/screens/Home/index.tsx
@@ -5,7 +5,11 @@ import { styles } from './styles';
 export default function Home() {
 
   function handleParticipantAdd() {
-    console.log("Botão de adicionar clicado.")
+    console.log("Botão de adicionar clicado.");
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Botão de remover clicado ${name}.`);
   }
 
   return (
@@ -32,10 +36,10 @@ export default function Home() {
         </TouchableOpacity>
       </View>
 
-      <Participant name="Cauã Campos"/>
-      <Participant name="Davi Garutti"/>
-      <Participant name="Iago (de Andrade Oliveira / de Oliveira Andrade)"/>
-      <Participant name="Dom Peso I"/>
+      <Participant name="Cauã Campos"  onRemove={() => handleParticipantRemove("Iago")}/>
+      <Participant name="Davi Garutti"  onRemove={() => handleParticipantRemove("Iago")}/>
+      <Participant name="Iago (de Andrade Oliveira / de Oliveira Andrade)" onRemove={() => handleParticipantRemove("Iago")}/>
+      <Participant name="Dom Peso I" onRemove={() => handleParticipantRemove("Iago")}/>
     </View>
   );
 }
